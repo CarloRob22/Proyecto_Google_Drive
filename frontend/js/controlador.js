9db2592ed6f9dc1cae859481278023c4bf3e5bf7
CarloRob22 <49041573+CarloRob22@users.noreply.github.com>
2020-08-23 04:59:37 -0600

    se agrego funcioines de obtener usuarios personales y grupales

--- frontend/js/controlador.js
+++ frontend/js/controlador.js
@@ -65,35 +65,24 @@ if (localStorage.getItem("archivos")==null){
     archivos = JSON.parse(localStorage.getItem('archivos'));
 }*/
 
-
-
-
-function generarArchivos(){
-    document.getElementById("id-archivos").innerHTML = '';
-    for (let i=0;i<archivos.length;i++) 
-        document.getElementById("id-archivos").innerHTML += 
-        `<tr>
-            <th id="Carpeta">${ValidarCarpeta(archivos[i].carpeta)}</th>            
-            <th id="Nombre">${archivos[i].Nombre}</th>
-            <td id="Propietario" >${archivos[i].Propietario}</td>
-            <td id="ultima-modif" >${archivos[i].ultima_modif}</td>
-            <td id="tamaño">${archivos[i].tamaño}</td>            
-        </tr>`;
-    console.log(archivos);
-}
-generarArchivos();
-
-function ValidarCarpeta(i){
-    if(i==true){   
-        return '<i class="fas fa-folder"></i>'
-
-    }else{
-        return '<i></i>'
-    };
-}
 var usuarios = [];
+var archivos = [];
+var archivosGrupales =[];
+var grupos =[];
 var usuarioSeleccionado;
 const url = '../../Proyecto_Google_Drive/backend/api/usuarios-personales.php';
+const url2= '../../Proyecto_Google_Drive/backend/api/usuarios-grupales.php';
+
+function obtenerTipoUsuario(){
+
+    if(document.getElementById('tipo-usuario').value=="personal"){
+        obtenerUsuariosPersonales();
+    }else{
+        obtenerUsuariosGrupales();
+    }
+    document.getElementById('tipo-usuario').value=null;
+}
+
 function obtenerUsuariosPersonales(){
     axios({
         method: 'GET',
@@ -108,25 +97,125 @@ function obtenerUsuariosPersonales(){
         console.error(error);
     });
 }
-obtenerUsuariosPersonales();
 
-function llenarDropdonw(){    
-    document.getElementById("lista-usuarios-personales").innerHTML= 
+
+function llenarDropdonw(){
+    document.getElementById("img-perfil").innerHTML=
+    `<img class= "" style="border-radius: 16px; height:32px; width:32px;" src="img/perfiles/pedro.jpg"></img>`;
+        
+    document.getElementById("lista-usuarios").innerHTML= 
     `<div id="encabezado-cuenta" class="dropdown-header" style="width: 100%;">
         
     </div>
     <div class="divider"></div>`;
     for(let i=0; i<usuarios.length; i++){
         console.log(i);
-        document.getElementById("lista-usuarios-personales").innerHTML+=
+        document.getElementById("lista-usuarios").innerHTML+=
         `<div class="dropdown-item">                                    
-            <a onclick="seleccionar(${i})" class="dropdown-item" href="#" style="padding-left: 0; ">
+            <a onclick="seleccionar(${i}); " class="dropdown-item" href="#" style="padding-left: 0; ">
                 <img class="" style="border-radius: 16px; margin-right: 10px; height:32px; width:32px;" src="${usuarios[i].foto}">
                 ${usuarios[i].nombre} ${usuarios[i].apellido}
             </a>                                                          
         </div>`;
-    }    
-    
+    }        
+}
+
+function obtenerUsuariosGrupales(){
+    axios({
+        method: 'GET',
+        url: url2,
+        responseType:'json',
+    }).then(res=>{
+        console.log(res.data);
+        this.grupos = res.data;               
+        llenarDropdownGrupos();
+    }).catch(error=>{
+        console.error(error);
+    });
+}
+
+function llenarDropdownGrupos(){
+    document.getElementById("img-perfil").innerHTML=
+    `<img class= "" style="border-radius: 16px; height:32px; width:32px;" src="img/perfiles/grupo-2.jpg"></img>`;
+        
+    document.getElementById("lista-usuarios").innerHTML= 
+    `<div id="encabezado-cuenta" class="dropdown-header" style="width: 100%;">
+        
+    </div>
+    <div class="divider"></div>`;
+    for(let i=0; i<grupos.length; i++){       
+        document.getElementById("lista-usuarios").innerHTML+=
+        `<div class="dropdown-item">                                    
+            <a onclick="seleccionarGrupo(${i}); " class="dropdown-item" href="#" style="padding-left: 0; ">
+                <img class="" style="border-radius: 16px; margin-right: 10px; height:32px; width:32px;" src="${grupos[i].fotoGrupo}">
+                ${grupos[i].nombreGrupo} 
+            </a>                                                          
+        </div>`;
+    }        
+}
+
+
+function obtenerArchivosPersonales(){
+    axios({
+        method: 'GET',
+        url: url + `?idArchivos=${usuarioSeleccionado}`,
+        responseType:'json',
+    }).then(res=>{
+        console.log(res.data);
+        this.archivos = res.data;               
+        llenarArchivos();
+    }).catch(error=>{
+        console.error(error);
+    });
+}
+
+function llenarArchivos(){    
+    document.getElementById("id-archivos").innerHTML = '';
+    for (let i=0;i<archivos.length;i++)        
+        document.getElementById("id-archivos").innerHTML += 
+        `<tr>
+            <th id="Carpeta">${ValidarCarpeta(archivos[i].carpeta)}</th>            
+            <th id="Nombre">${archivos[i].nombre}</th>
+            <!--<td id="Propietario" >{obtenerUsuario(archivos[i].idUsuario)}</td>-->
+            <td id="ultima-modif" >${archivos[i].ultima_modif}</td>
+            <td id="tamaño">${archivos[i].tamano}</td>            
+        </tr>`;
+}
+
+function obtenerArchivosGrupales(){
+    axios({
+        method: 'GET',
+        url: url + `?idArchivos=${usuarioSeleccionado}`,
+        responseType:'json',
+    }).then(res=>{
+        console.log(res.data);
+        this.archivosGrupales = res.data;               
+        llenarArchivosGrupales();
+    }).catch(error=>{
+        console.error(error);
+    });
+}
+
+function llenarArchivosGrupales(){    
+    document.getElementById("id-archivos").innerHTML = '';
+    for (let i=0;i<archivosGrupales.length;i++)        
+        document.getElementById("id-archivos").innerHTML += 
+        `<tr>
+            <th id="Carpeta">${ValidarCarpeta(archivosGrupales[i].carpeta)}</th>            
+            <th id="Nombre">${archivosGrupales[i].nombre}</th>
+            <!--<td id="Propietario" >{obtenerUsuario(archivos[i].idUsuario)}</td>-->
+            <td id="ultima-modif" >${archivosGrupales[i].ultima_modif}</td>
+            <td id="tamaño">${archivosGrupales[i].tamano}</td>            
+        </tr>`;
+}
+
+function ValidarCarpeta(i){
+    if(i==true){   
+        return '<i class="fas fa-folder"></i>'
+
+    }else{
+        return '<i></i>'
+    };
 }
 
 function seleccionar(indice){
@@ -142,7 +231,29 @@ function seleccionar(indice){
         `<img id="img-drop-perfil" style="border-radius: 16px; height:32px; width:32px;" src="${res.data.foto}">                              
          <p align="center" id="nombre">${res.data.nombre} ${res.data.apellido}</p>
          <h6 align="center" id="nombre-usuario">${res.data.nombreUsuario}</h6>`;       
-                
+         obtenerArchivosPersonales(res.data.idUsuario);
+         document.getElementById("img-perfil").innerHTML=
+        `<img class="" style="border-radius: 16px; margin-right: 10px; height:32px; width:32px;" src="${res.data.foto}">`;      
+    }).catch(error=>{
+        console.error(error);
+    });    
+}
+
+function seleccionarGrupo(indice){
+    indice=indice+1;
+    usuarioSeleccionado = indice;
+    axios({
+        method: 'GET',
+        url: url2 + `?idGrupo=${indice}`,
+        responseType:'json',       
+    }).then(res=>{        
+        console.log(res);  
+        document.getElementById('encabezado-cuenta').innerHTML=
+        `<img id="img-drop-perfil" style="border-radius: 16px; height:32px; width:32px;" src="${res.data.fotoGrupo}">                               
+         <h6 align="center" id="nombre-usuario">${res.data.nombreGrupo}</h6>`;       
+         obtenerArchivosGrupales(res.data.idUsuario);
+         document.getElementById("img-perfil").innerHTML=
+        `<img class="" style="border-radius: 16px; margin-right: 10px; height:32px; width:32px;" src="${res.data.fotoGrupo}">`;      
     }).catch(error=>{
         console.error(error);
     });    
